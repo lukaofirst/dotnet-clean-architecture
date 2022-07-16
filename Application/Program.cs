ca02f368f4c1670ebd359e03d37ed036b08c83bd
lukaofirst <lucasmacedo123@gmail.com>
2022-07-15 22:43:27 -0300

    adding IoC layer and services to the project

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,3 +1,6 @@
+using Core.Settings;
+using IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Adding the SettingConfig class
+builder.Services.Configure<SettingsConfig>(builder.Configuration.GetSection("SettingsConfig"));
+
+builder.Services.AddMvc().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNameCaseInsensitive = true);
+
+DependencyContainer.InjectServices(builder.Services);
 
 var app = builder.Build();
 
---
+++ IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Core.Interfaces.Repositories;
+using Infraestructure.Data;
+using Infraestructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IoC
+{
+    public class DependencyContainer
+    {
+        public static void InjectServices(IServiceCollection services)
+        {
+            services.AddSingleton<MongoDBContext>();
+            services.AddScoped<IPersonRepository, PersonRepository>();
+        }
+    }
+}
