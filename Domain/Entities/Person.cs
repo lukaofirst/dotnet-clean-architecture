2ff75b058c69568fa8bfdedceb8c9249c077e0f3
lukaofirst <lucasmacedo123@gmail.com>
2024-12-27 11:24:11 -0300

    updating Domain's layer

---
+++ Domain/Entities/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Person
+{
+	public object? _id { get; set; }
+	public string? name { get; set; }
+	public int age { get; set; }
+	public string? job { get; set; }
+}
---
+++ Domain/Interfaces/Repositories/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Repositories;
+
+public interface IPersonRepository
+{
+	Task<List<Person>> GetAll();
+	Task<Person> InsertOne(Person person);
+	Task<Person> UpsertOne(Person person);
+	Task<bool> DeleteOne(string objectId);
+}
