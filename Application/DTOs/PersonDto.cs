60b25b7313d0a0dffb84e3b738d179f622bc8841
lukaofirst <lucasmacedo123@gmail.com>
2024-12-27 14:51:44 -0300

    renaming PersonDto's file

---
+++ Application/DTOs/PersonDto.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs;
+
+public class PersonDto
+{
+	[BsonId]
+	[BsonRepresentation(BsonType.ObjectId)]
+	public Guid Id { get; set; }
+
+	[BsonElement("name")]
+	[Required]
+	[StringLength(120)]
+	public string Name { get; set; } = string.Empty;
+
+	[BsonElement("age")]
+	[Range(0, 99)]
+	public int Age { get; set; }
+
+	[BsonElement("job")]
+	[BsonIgnoreIfNull]
+	public string? Job { get; set; }
+}
